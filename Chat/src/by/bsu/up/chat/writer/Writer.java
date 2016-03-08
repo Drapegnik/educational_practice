53e4e16776f80304683fb706dbf4ff7f1feaf144
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-03-08 13:55:32 +0300

    add chat server

---
+++ Chat/src/by/bsu/up/chat/writer/Writer.java
@@ -0,0 +1,7 @@
+package by.bsu.up.chat.writer;
+
+/**
+ * Created by Drapegnik on 07.03.16.
+ */
+public class Writer {
+}
---
+++ CleanCode/src/by/bsu/famcs/drapegnik/cleancode/History.java
@@ -0,0 +1,15 @@
+package by.bsu.famcs.drapegnik.cleancode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Drapegnik on 23.02.16.
+ */
+public class History {
+    private List<JsonMessage> history;
+
+    public History() {
+        history = new ArrayList<>();
+    }
+}
