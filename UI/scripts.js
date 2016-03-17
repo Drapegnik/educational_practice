42019b00b0e9fb7b1e8f44e8edd55fee660e1b72
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-03-17 11:10:30 +0300

    add local storaging

--- UI/scripts.js
+++ UI/scripts.js
@@ -2,8 +2,20 @@
  * Created by Drapegnik on 03.03.16.
  */
 
-var name = "User";
+var name = "";
 var id = 0;
+
+var messageList = [];
+
+function run() {
+    messageList = loadMessages() || [
+            newMes('Hi')
+        ];
+    id = messageList[messageList.length - 1].id;
+    name = loadUsername() || "User";
+    document.getElementById("name").value = name;
+    render(messageList);
+}
 
 function changeName() {
     var inputName = document.getElementById("name");
@@ -26,6 +38,7 @@ function changeName() {
     }
     else if (name != inputName.value.trim()) {
         name = inputName.value.trim();
+        saveUsername(name);
 
         var messages = document.getElementsByTagName("li");
         var times = document.getElementsByClassName("time");
@@ -47,68 +60,17 @@ function send() {
         message.classList.add("holdcol");
     }
     else {
-        var time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
-        var li = document.createElement("li");
-        li.setAttribute("id", "id" + id);
-        li.classList.add("message", "in");
-        li.innerHTML = "<a href='#' onclick='deleteMes(\"" + id + "\")'><i class='fa fa-trash'></i></a> " +
-            "<a href='#' onclick='editMes(\"" + id + "\")'><i class='fa fa-pencil'></i></a>";
-
-        var div = document.createElement("div");
-        div.classList.add("text");
-        div.setAttribute("id", "text" + id);
-        div.textContent = message.value;
-
-        var input = document.createElement("input");
-        input.setAttribute("type", "text");
-        input.setAttribute("id", "input" + id);
-        input.setAttribute("name", "edit");
-        input.classList.add("editInput");
-        input.hidden = true;
-        input.onkeypress = function () {
-            enter(event)
-        };
-        input.addEventListener("focusout", function () {
-            cancelMes(input.id);
-        });
-
-        var p_sign = document.createElement("p");
-        p_sign.textContent = name;
-        p_sign.classList.add("sign", "myname");
-
-        var p_time = document.createElement("p");
-        p_time.textContent = time;
-        p_time.setAttribute("id", "time" + id);
-        p_time.classList.add("time", "in");
-
-        var cancel = document.createElement("span");
-        cancel.innerHTML = "<a href='#' style='color: #d9534f;' onclick='cancelMes(\"" + id + "\")'><i class='fa fa-times'></i></a>";
-        cancel.firstElementChild.hidden = true;
-
-        li.appendChild(input);
-        li.appendChild(cancel);
-        li.appendChild(div);
-        li.appendChild(p_sign);
-
-        document.getElementById("chat").appendChild(li);
-        document.getElementById("chat").appendChild(p_time);
-        document.location.href = "#id" + id;
-        id++;
+        messageList.push(newMes(message.value));
+        render(messageList);
     }
     message.value = "";
 }
 
 function deleteMes(id) {
-    var text = document.getElementById("text" + id);
-    text.classList.add("delete");
-    text.textContent = "message was delete";
+    messageList[id - 1].isDelete = true;
+    messageList[id - 1].time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    render(messageList);
 
-    var childs = document.getElementById("id" + id).childNodes;
-    childs[0].hidden = true;
-    childs[2].hidden = true;
-
-    var time = document.getElementById("time" + id);
-    time.textContent = "delete on " + new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
 }
 
 function editMes(id) {
@@ -130,26 +92,24 @@ function saveMes(inputId) {
         input.classList.add("holdcol");
     }
     else {
-        var text = document.getElementById("text" + inputId.substr(5));
-        var time = document.getElementById("time" + inputId.substr(5));
-
-        text.textContent = input.value;
+        messageList[inputId.substr(5) - 1].text = input.value;
+        messageList[inputId.substr(5) - 1].time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+        messageList[inputId.substr(5) - 1].isEdit = true;
         input.hidden = true;
-        text.hidden = false;
         input.nextElementSibling.firstElementChild.hidden = true;
 
-        time.textContent = "edit on " + new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+        render(messageList);
     }
 }
 
 function cancelMes(inputId) {
     var input = document.getElementById(inputId);
-    var text = document.getElementById("text" + inputId.substr(5));
-    input.value = text.textContent;
+    input.value = messageList[inputId.substr(5) - 1].text;
 
     input.hidden = true;
-    text.hidden = false;
     input.nextElementSibling.firstElementChild.hidden = true;
+
+    render(messageList);
 }
 
 function enter(e) {
@@ -167,3 +127,139 @@ document.onkeydown = function (e) {
         if (e.keyCode == 27)
             cancelMes(e.srcElement.id);
 };
+
+function render(messages) {
+    document.getElementById("chat").innerHTML = "";
+    for (var i = 0; i < messages.length; i++)
+        renderMes(messages[i]);
+    document.location.href = "#id" + id;
+    saveMessages(messages);
+}
+
+function renderMes(mes) {
+    var li = document.createElement("li");
+    li.setAttribute("id", "id" + mes.id);
+    li.innerHTML = "<a href='#' onclick='deleteMes(\"" + mes.id + "\")'><i class='fa fa-trash'></i></a> " +
+        "<a href='#' onclick='editMes(\"" + mes.id + "\")'><i class='fa fa-pencil'></i></a>";
+
+    var div = document.createElement("div");
+    div.classList.add("text");
+    div.setAttribute("id", "text" + mes.id);
+    div.textContent = mes.text;
+
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "input" + mes.id);
+    input.setAttribute("name", "edit");
+    input.classList.add("editInput");
+    input.hidden = true;
+    input.onkeypress = function () {
+        enter(event)
+    };
+    input.addEventListener("focusout", function () {
+        cancelMes(input.id);
+    });
+
+    var p_sign = document.createElement("p");
+    p_sign.textContent = mes.user;
+    p_sign.classList.add("sign", "myname");
+
+    var p_time = document.createElement("p");
+    p_time.textContent = mes.time;
+    p_time.setAttribute("id", "time" + mes.id);
+
+
+    if (mes.user == name) {
+        li.classList.add("message", "in");
+        p_time.classList.add("time", "in");
+    }
+    else {
+        li.classList.add("message", "out");
+        p_time.classList.add("time", "out");
+    }
+
+    var cancel = document.createElement("span");
+    cancel.innerHTML = "<a href='#' style='color: #d9534f;' onclick='cancelMes(\"" + mes.id + "\")'><i class='fa fa-times'></i></a>";
+    cancel.firstElementChild.hidden = true;
+
+    li.appendChild(input);
+    li.appendChild(cancel);
+    li.appendChild(div);
+    li.appendChild(p_sign);
+
+    renderMesState(li, p_time, mes);
+
+    document.getElementById("chat").appendChild(li);
+    document.getElementById("chat").appendChild(p_time);
+}
+
+
+function renderMesState(li, p_time, mes) {
+    if (mes.isDelete || mes.user != name) {
+        if (mes.isDelete) {
+            var text = li.getElementsByClassName("text")[0];
+            text.classList.add("delete");
+            text.textContent = "message was delete";
+            p_time.textContent = "delete on " + mes.time;
+        }
+
+        li.childNodes[0].hidden = true;
+        li.childNodes[2].hidden = true;
+    } else if (mes.isEdit)
+        p_time.textContent = "edit on " + mes.time;
+}
+
+
+function newMes(text) {
+    id++;
+    return {
+        user: name,
+        id: id,
+        text: text,
+        time: new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"),
+        isDelete: false,
+        isEdit: false
+    };
+}
+
+function saveMessages(listToSave) {
+    if (!isLocStorOk())
+        return;
+
+    localStorage.setItem("Chat History", JSON.stringify(listToSave));
+}
+
+function saveUsername(name) {
+    if (!isLocStorOk())
+        return;
+
+    console.log(name);
+    localStorage.setItem("Username", name);
+}
+
+function loadMessages() {
+    if (!isLocStorOk())
+        return;
+
+    var item = localStorage.getItem("Chat History");
+
+    return item && JSON.parse(item);
+}
+
+function loadUsername() {
+    if (!isLocStorOk())
+        return;
+
+    var item = localStorage.getItem("Username");
+    console.log(item);
+    return item;
+}
+
+function isLocStorOk() {
+    if (typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return false;
+    }
+    else
+        return true;
+}
